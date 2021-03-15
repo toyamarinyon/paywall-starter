e0a84194d1284a6d35fcffe48b6b5e20278bf66f
toyama satoshi <toyamarinyon@gmail.com>
2021-03-15 11:10:51 +0900

    Add favicon

--- packages/storefront/pages/_app.tsx
+++ packages/storefront/pages/_app.tsx
@@ -1,9 +1,14 @@
 import { Provider } from "next-auth/client";
+import Head from "next/head";
 import "tailwindcss/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider session={pageProps.session}>
+      <Head>
+        <title>PAYWALL</title>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
     </Provider>
   );
